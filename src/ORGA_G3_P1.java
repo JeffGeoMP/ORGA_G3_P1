e8ea07f791622fea07f2b67047d58f677c8c38a9
Jefferson Geovanny Moreno Perez <34329727+Jefferson201603047@users.noreply.github.com>
2019-10-02 09:10:55 -0600

    Main de Proyecto

---
+++ src/ORGA_G3_P1.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author JeffGeo
+ */
+public class ORGA_G3_P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
